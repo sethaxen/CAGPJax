6f803ead6a982d2512d90f8ee4556fa593d3d1b0
Seth Axen <seth@sethaxen.com>
2025-06-24 16:18:02 +0200

    Add LanczosPolicy to public API

--- cagpjax/__init__.py
+++ cagpjax/__init__.py
@@ -1,8 +1,10 @@
 """Computation-Aware Gaussian Processes for GPJax."""
 
 from cagpjax.operators import BlockDiagonalSparse
+from cagpjax.policies import LanczosPolicy
 
 __version__ = "0.1.0"
 __all__ = [
     "BlockDiagonalSparse",
+    "LanczosPolicy",
 ]
--- cagpjax/policies/__init__.py
+++ cagpjax/policies/__init__.py
@@ -1,6 +1,8 @@
 from .base import AbstractBatchLinearSolverPolicy, AbstractLinearSolverPolicy
+from .lanczos import LanczosPolicy
 
 __all__ = [
     "AbstractLinearSolverPolicy",
     "AbstractBatchLinearSolverPolicy",
+    "LanczosPolicy",
 ]
