066dfff041de3026dfe88ee6ea94ddea6a75a040
Seth Axen <seth@sethaxen.com>
2025-06-26 10:46:23 +0200

    Add abstract base class for CAGP

---
+++ cagpjax/models.py
@@ -0,0 +1,14 @@
+import abc
+
+from gpjax.variational_families import AbstractVariationalFamily
+
+
+class AbstractComputationallyAwareGP(AbstractVariationalFamily, abc.ABC):
+    """Abstract base class for Computationally-Aware Gaussian Processes.
+
+    While CaGPs can be viewed as exact GPs on a data subspace, when the actions
+    are learnable, they can also be interpreted as a variational family whose
+    variational parameters are the parameters of the actions.
+    """
+
+    ...
