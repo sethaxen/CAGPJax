ffbbbfcdae02fe708ab6bb372ac1a038b9e39bcc
Seth Axen <seth@sethaxen.com>
2025-07-18 10:08:10 +0200

    Add Cholesky solver

--- src/cagpjax/solvers/__init__.py
+++ src/cagpjax/solvers/__init__.py
@@ -1,6 +1,8 @@
 from .base import AbstractLinearSolver, AbstractLinearSolverMethod
+from .cholesky import Cholesky
 
 __all__ = [
     "AbstractLinearSolver",
     "AbstractLinearSolverMethod",
+    "Cholesky",
 ]
