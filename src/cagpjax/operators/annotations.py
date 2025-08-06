18a8e6ff4019d33ddc3fb5c3744355495021e6ac
Seth Axen <seth@sethaxen.com>
2025-08-06 21:41:45 +0200

    Add ScaledOrthogonal annotation

---
+++ src/cagpjax/operators/annotations.py
@@ -0,0 +1,9 @@
+"""Annotations for operators."""
+
+import cola
+
+
+class ScaledOrthogonal(cola.annotations.Annotation):
+    """Annotation for an operator whose columns are orthogonal (but not necessarily orthonormal)."""
+
+    pass
