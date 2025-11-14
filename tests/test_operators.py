8918fa5a6c894d94b8cc668ee18e2fc2082f0aa9
Seth Axen <seth@sethaxen.com>
2025-11-14 11:44:00 +0100

    Allow setting linop consistency check tolerance

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -15,21 +15,21 @@ from cagpjax.operators.lazy_kernel import LazyKernel
 jax.config.update("jax_enable_x64", True)
 
 
-def _test_mul_consistency(op: LinearOperator):
+def _test_mul_consistency(op: LinearOperator, **kwargs):
     """Test that the linear operator is consistent with multiplication by identity."""
     mat = op.to_dense()
-    np.testing.assert_allclose(jnp.eye(op.shape[0]) @ op, mat)
-    np.testing.assert_allclose(op @ jnp.eye(op.shape[1]), mat)
+    np.testing.assert_allclose(jnp.eye(op.shape[0]) @ op, mat, **kwargs)
+    np.testing.assert_allclose(op @ jnp.eye(op.shape[1]), mat, **kwargs)
 
 
-def _test_transpose_consistency(op: LinearOperator):
+def _test_transpose_consistency(op: LinearOperator, **kwargs):
     """Test that the transpose is consistent."""
     op_transpose = op.T
     assert op_transpose.shape == op.shape[::-1]
-    np.testing.assert_allclose(op_transpose.to_dense(), op.to_dense().T)
+    np.testing.assert_allclose(op_transpose.to_dense(), op.to_dense().T, **kwargs)
 
 
-def _test_dtype_consistency(op: LinearOperator):
+def _test_dtype_consistency(op: LinearOperator, **kwargs):
     """Test that the linear operator has the correct dtype."""
     assert op.dtype == op.to_dense().dtype
     x = jnp.ones(op.shape[1], dtype=op.dtype)
@@ -38,11 +38,11 @@ def _test_dtype_consistency(op: LinearOperator):
     assert (op.T @ y).dtype == op.dtype
 
 
-def _test_linear_operator_consistency(op: LinearOperator):
+def _test_linear_operator_consistency(op: LinearOperator, **kwargs):
     """Test that the linear operator is self-consistent."""
-    _test_mul_consistency(op)
-    _test_transpose_consistency(op)
-    _test_dtype_consistency(op)
+    _test_mul_consistency(op, **kwargs)
+    _test_transpose_consistency(op, **kwargs)
+    _test_dtype_consistency(op, **kwargs)
 
 
 class TestBlockDiagonalSparse:
